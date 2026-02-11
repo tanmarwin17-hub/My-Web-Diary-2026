eb3fa58cc115792d9d5a24f7799f87b66a4d1235
Marwin Tan <tan.marwin17@gmail.com>
2026-02-11 08:09:01 +0800

    Practical Exam Part 1

--- src/diary/Diary.tsx
+++ src/diary/Diary.tsx
@@ -19,7 +19,7 @@ export type DiaryEntryType = {
     title: string,
     mood: number,
     content: string,
-    
+    star: number, 
 }
 
 export const moodList: MoodType[] = [
@@ -71,30 +71,35 @@ export const sampleDiary: DiaryEntryType[] = [
         mood: 1,
         date: new Date(),
         title: 'My first entry',
-        content: 'My first entry din.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        content: 'My first entry din. There are so many ways to show your significant other just how much they mean to you—but that doesnt mean its always easy. Sometimes it can be hard to choose a sentimental gift, especially when youve been together as long as The Pioneer Woman and her Marlboro Man. And sometimes, life just gets in the way! Ree once said, "When Ladd and I are both painfully busy and going in a million separate directions, we keep our heads down and power through—but we are at our best when we have time together." And when youve found the one who is meant to be yours, isnt that so true?',
+        star: 5
     },
     {
         mood: 2,
         date: new Date(),
         title: 'My first sad entry',
-        content: 'My first entry din. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
+        content: 'Still, no matter the time and distance, you want them to know you feel all the mushy and gushy things for them. After all, theres little that feels better than making your special someone happy. And the most traditional way of doing that? With romantic love messages, especially as Valentines Day approaches! Here, weve come up with the best love messages, including the cutest couple quotes and quotes about love to share with the one who holds your heart.',
+        star: 3
     },
     {
         mood: 3,
         date: new Date(),
         title: 'My first entry',
-        content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+        content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+        star: 4
     },
     {
         mood: 4,
         date: new Date(),
         title: 'My first hunger, silent h',
-        content: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+        content: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+        star: 2
     },
     {
         mood: 5,
         date: new Date(),
         title: 'My first hunger',
-        content: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.'
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        star: 1
     },
 ]
--- src/diary/DiaryList.tsx
+++ src/diary/DiaryList.tsx
@@ -6,43 +6,46 @@ import { moodList, sampleDiary, type DiaryEntryType } from "./Diary"
 import { useState } from "react"
 
 function DiaryList() {
-
     const diarylist = sampleDiary
 
     return (
-        <>
+        <Box sx={{ p: 2 }}>
             {diarylist.map((entry, index) => (
                 <DiaryEntry entry={entry} key={index} />
             ))}
-        </>
+        </Box>
     )
 }
 
 function DiaryEntry(prop: { entry: DiaryEntryType }) {
-
     const { entry } = prop
-
     const [expand, setExpand] = useState(false)
+
+   
+    const starIcons = "★".repeat(entry.star)
 
     return (
         <Paper elevation={1} sx={{
             display: 'flex',
             p: 1,
             backgroundColor: blue[100],
+            mb: 1 
         }}>
 
             <Typography sx={{ fontSize: '48px' }}>
                 {moodList[entry.mood].icon}
             </Typography>
+
             <Box sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 pl: 1,
+                flexGrow: 1, 
             }}>
                 <Typography sx={{ textAlign: 'left' }}>
                     {entry.date.toUTCString()}
                 </Typography>
-                <Typography onClick={() => setExpand(!expand)}>
+                <Typography onClick={() => setExpand(!expand)} sx={{ cursor: 'pointer' }}>
                     {entry.title}
                 </Typography>
                 {expand && (
@@ -50,11 +53,28 @@ function DiaryEntry(prop: { entry: DiaryEntryType }) {
                         {entry.content}
                     </Typography>
                 )}
-
             </Box>
+
+            {
+            }
+            {/* Gradient Star Display */}
+            <Typography sx={{ 
+                fontSize: '24px', 
+                pl: 1, 
+                alignSelf: 'flex-start',
+                fontWeight: 'bold',
+                // Gradient Logic:
+                background: 'linear-gradient(90deg, rgba(131, 58, 180, 1) 0%, rgba(253, 29, 29, 1) 50%, rgba(252, 176, 69, 1) 100%)',
+                WebkitBackgroundClip: 'text',
+                WebkitTextFillColor: 'transparent',
+                // Fallback for older browsers
+                display: 'inline-block'
+            }}>
+                {starIcons}
+            </Typography>
+
         </Paper>
     )
-
 }
 
-export default DiaryList 
+export default DiaryList
